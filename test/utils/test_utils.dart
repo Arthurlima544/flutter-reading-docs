76ebf05de25d65559301367cd120306507e93b67
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-27 09:29:49 -0300

    fix: reducing options from 220 to 15

--- test/utils/test_utils.dart
+++ test/utils/test_utils.dart
@@ -9,41 +9,12 @@ List<double> commonAspectRatios = [
   21 / 9,
 ];
 
-List<double> commonDPRs = [
-  0.5,
-  0.75,
-  1.0,
-  1.25,
-  1.5,
-  1.75,
-  2.0,
-  2.5,
-  3.0,
-  3.5,
-  4.0,
-];
+List<double> commonDPRs = [1.0, 1.5, 2.0];
 
 Map<String, List<Size>> devicesSizes = {
-  "watches": [Size(136, 170), Size(162, 197), Size(184, 224)],
-  "phones": [
-    Size(360, 640),
-    Size(375, 812),
-    Size(390, 844),
-    Size(412, 915),
-    Size(428, 926),
-  ],
-  "tablets": [
-    Size(600, 1024),
-    Size(768, 1024),
-    Size(834, 1112),
-    Size(820, 1180),
-  ],
-  "notebooks": [
-    Size(1280, 800),
-    Size(1366, 768),
-    Size(1440, 900),
-    Size(1536, 864),
-    Size(1920, 1080),
-  ],
-  "bigscreens": [Size(1280, 720), Size(1920, 1080), Size(3840, 2160)],
+  "watches": [Size(162, 197)],
+  "phones": [Size(390, 844)],
+  "tablets": [Size(768, 1024)],
+  "notebooks": [Size(1440, 900)],
+  "bigscreens": [Size(3840, 2160)],
 };
